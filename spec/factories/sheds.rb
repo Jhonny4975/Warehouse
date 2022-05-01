646ae176f8f92146f7039884c6f24ef477a9d1c3
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-01 17:40:36 -0300

    Update sheds.rb

--- spec/factories/sheds.rb
+++ spec/factories/sheds.rb
@@ -2,5 +2,9 @@
 
 FactoryBot.define do
   factory :shed do
+    name { Faker::Company.name }
+    code { Faker::Address.country_code_long }
+    city { Faker::Address.city }
+    area { Faker::Number.decimal(l_digits: 2, r_digits: 3) }
   end
 end
