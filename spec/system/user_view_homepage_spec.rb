b6e6c1defb97100ddf9c5157c13e3d093b2485aa
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-01 18:28:09 -0300

    increase tests

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -3,21 +3,38 @@
 require 'rails_helper'
 
 describe 'User visits home screen' do
-  let(:shed) { create(:shed) }
+  subject(:home) { visit('/') }
 
-  before do
-    shed
-    visit('/')
+  context 'when there are sheds' do
+    let(:shed) { create(:shed) }
+
+    before do
+      shed
+      home
+    end
+
+    it 'must see app name' do
+      expect(page).to have_content('Galpões & Estoque')
+    end
+
+    it 'the user should see the registered sheds' do
+      expect(page).not_to have_content('there is no registered shed')
+      expect(page).to have_content(shed.name)
+      expect(page).to have_content("code: #{shed.code}")
+      expect(page).to have_content("city: #{shed.city}")
+      expect(page).to have_content("#{shed.city} m2")
+    end
   end
 
-  it 'must see app name' do
-    expect(page).to have_content('Galpões & Estoque')
-  end
+  context 'when there are no sheds' do
+    before { home }
 
-  it 'the user should see the registered sheds' do
-    expect(page).to have_content(shed.name)
-    expect(page).to have_content("code: #{shed.code}")
-    expect(page).to have_content("city: #{shed.city}")
-    expect(page).to have_content("#{shed.city} m2")
+    it 'must see app name' do
+      expect(page).to have_content('Galpões & Estoque')
+    end
+
+    it 'User sees a message' do
+      expect(page).to have_content('there is no registered shed')
+    end
   end
 end
