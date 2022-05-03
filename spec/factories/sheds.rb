bed91b9d6aba422dfa5cc3d6816e92e0808e7fe3
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-03 14:59:45 -0300

    creating tests for shed details

--- spec/factories/sheds.rb
+++ spec/factories/sheds.rb
@@ -6,5 +6,8 @@ FactoryBot.define do
     code { Faker::Address.country_code_long }
     city { Faker::Address.city }
     area { Faker::Number.decimal(l_digits: 2, r_digits: 3) }
+    address { Faker::Address.street_address }
+    postcode { Faker::Address.postcode }
+    description { Faker::Lorem.paragraph }
   end
 end
