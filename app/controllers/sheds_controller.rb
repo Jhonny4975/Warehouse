55f84cd6642ff939f9b3ee8f27acfd8aa7450156
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-04 15:57:20 -0300

    Update sheds_controller.rb

--- app/controllers/sheds_controller.rb
+++ app/controllers/sheds_controller.rb
@@ -1,7 +1,33 @@
 # frozen_string_literal: true
 
 class ShedsController < ApplicationController
+  def new
+    @shed = Shed.new
+  end
+
   def show
     @shed = Shed.find(params[:id])
+  end
+
+  def create
+    Shed.create!(shed_params).then do |shed|
+      flash[:notice] = 'shed successfully registered!'
+
+      redirect_to shed_path(shed.id)
+    end
+  end
+
+  private
+
+  def shed_params
+    params.require(:shed).permit(
+      :name,
+      :code,
+      :city,
+      :area,
+      :address,
+      :postcode,
+      :description
+    )
   end
 end
