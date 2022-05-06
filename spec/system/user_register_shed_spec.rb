15b991e22e8cddaf33085f551857a7aaf263bf39
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-06 10:07:18 -0300

    fix AAA offenses

--- spec/system/user_register_shed_spec.rb
+++ spec/system/user_register_shed_spec.rb
@@ -3,26 +3,10 @@
 require 'rails_helper'
 
 describe 'User accesses warehouse register' do
-  subject(:register) { click_on 'register shed' }
-
-  let(:home) { visit root_path }
-  let(:post) do
-    fill_in 'Name:', with: Faker::Company.name
-    fill_in 'Description:', with: Faker::Address.country_code_long
-    fill_in 'Code:', with: Faker::Address.city
-    fill_in 'Address:', with: Faker::Number.decimal(l_digits: 2, r_digits: 3)
-    fill_in 'City:', with: Faker::Address.street_address
-    fill_in 'Postcode:', with: Faker::Address.postcode
-    fill_in 'Area:', with: Faker::Lorem.paragraph
-    click_on 'Save'
-  end
-
-  before do
-    home
-    register
-  end
-
   it 'and should see registration fields' do
+    visit root_path
+    click_on 'register shed'
+
     expect(page).to have_current_path new_shed_path
     expect(page).to have_field 'Name:'
     expect(page).to have_field 'Description:'
@@ -34,7 +18,16 @@ describe 'User accesses warehouse register' do
   end
 
   it 'and register a shed' do
-    post
+    visit root_path
+    click_on 'register shed'
+    fill_in 'Name:', with: Faker::Company.name
+    fill_in 'Description:', with: Faker::Address.country_code_long
+    fill_in 'Code:', with: Faker::Address.city
+    fill_in 'Address:', with: Faker::Number.decimal(l_digits: 2, r_digits: 3)
+    fill_in 'City:', with: Faker::Address.street_address
+    fill_in 'Postcode:', with: Faker::Address.postcode
+    fill_in 'Area:', with: Faker::Lorem.paragraph
+    click_on 'Save'
 
     expect(page).to have_current_path shed_path(Shed.last.id)
     expect(page).to have_content 'shed successfully registered!'
