7efc5cd18b3bea572aa1c08e17a3d70f9f098628
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-07 14:54:28 -0300

    add validations for all shed attributes

--- app/models/shed.rb
+++ app/models/shed.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class Shed < ApplicationRecord
+  validates :name,
+            :code,
+            :city,
+            :area,
+            :address,
+            :postcode,
+            :description, presence: true
+  validates :code, length: { is: 3 }
 end
