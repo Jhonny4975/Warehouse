12f3aa44a583c73eb2955b8f9eccf92edb77628a
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-07 14:53:45 -0300

    increase tests with validations tests

--- spec/system/user_register_shed_spec.rb
+++ spec/system/user_register_shed_spec.rb
@@ -17,19 +17,50 @@ describe 'User accesses warehouse register' do
     expect(page).to have_field 'Area:'
   end
 
-  it 'and register a shed' do
-    visit root_path
-    click_on 'register shed'
-    fill_in 'Name:', with: Faker::Company.name
-    fill_in 'Description:', with: Faker::Address.country_code_long
-    fill_in 'Code:', with: Faker::Address.city
-    fill_in 'Address:', with: Faker::Number.decimal(l_digits: 2, r_digits: 3)
-    fill_in 'City:', with: Faker::Address.street_address
-    fill_in 'Postcode:', with: Faker::Address.postcode
-    fill_in 'Area:', with: Faker::Lorem.paragraph
-    click_on 'Save'
+  context 'when to fill in the fields' do
+    it 'with valid params' do
+      visit root_path
+      click_on 'register shed'
+      fill_in 'Name:', with: Faker::Company.name
+      fill_in 'Code:', with: Faker::Address.country_code_long
+      fill_in 'City:', with: Faker::Address.city
+      fill_in 'Area:', with: Faker::Number.decimal(l_digits: 2, r_digits: 3)
+      fill_in 'Address:', with: Faker::Address.street_address
+      fill_in 'Postcode:', with: Faker::Address.postcode
+      fill_in 'Description:', with: Faker::Lorem.paragraph
+      click_on 'Save'
 
-    expect(page).to have_current_path shed_path(Shed.last.id)
-    expect(page).to have_content 'shed successfully registered!'
+      expect(page).to have_current_path shed_path(Shed.last.id)
+      expect(page).to have_content 'shed successfully registered!'
+    end
+
+    it 'with empty params' do
+      visit root_path
+      click_on 'register shed'
+      fill_in 'Name:', with: ''
+      fill_in 'Description:', with: ''
+      click_on 'Save'
+
+      expect(page).to have_current_path sheds_path
+      expect(page).to have_content 'invalid information, review the fields'
+    end
+
+    it 'with invalid params' do
+      invalid_field = 'invalid value'
+
+      visit root_path
+      click_on 'register shed'
+      fill_in 'Name:', with: Faker::Company.name
+      fill_in 'Description:', with: Faker::Address.country_code_long
+      fill_in 'Code:', with: invalid_field
+      fill_in 'Address:', with: invalid_field
+      fill_in 'City:', with: Faker::Address.street_address
+      fill_in 'Postcode:', with: invalid_field
+      fill_in 'Area:', with: invalid_field
+      click_on 'Save'
+
+      expect(page).to have_current_path sheds_path
+      expect(page).to have_content 'invalid information, review the fields'
+    end
   end
 end
