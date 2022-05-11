db22f2a151c5db2d7adf578775b474cbb00db430
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-11 13:58:37 -0300

    increase tests fot update shed

--- spec/models/shed_spec.rb
+++ spec/models/shed_spec.rb
@@ -104,8 +104,9 @@ RSpec.describe Shed, type: :model do
       end
 
       it 'invalid when name is already in use' do
-        create(:shed, name: 'ASU')
-        second_shed = described_class.new(attributes_for(:shed, name: 'ASU'))
+        first_shed = create(:shed, name: 'SP')
+        second_shed = described_class.new(attributes_for(:shed, name: 'SP'))
+
 
         expect(second_shed).not_to be_valid
         expect(second_shed.errors.full_messages.length).to eq 1
@@ -146,6 +147,136 @@ RSpec.describe Shed, type: :model do
 
       it 'invalid when postcode does not match format' do
         shed = described_class.new(attributes_for(:shed, postcode: '451165415469'))
+
+        expect(shed).not_to be_valid
+        expect(shed.errors.full_messages.length).to eq 1
+        expect(shed.errors.full_messages.last).to eq 'Código postal deve ter o formato: 00000-000'
+      end
+    end
+
+    context 'when editing a shed' do
+      it 'invalid when name is not present' do
+        shed = create(:shed)
+
+        shed.update(name: '')
+
+        expect(shed).not_to be_valid
+        expect(shed.errors.full_messages.length).to eq 1
+        expect(shed.errors.full_messages.last).to eq 'Nome não pode ficar em branco'
+      end
+
+      it 'invalid when code is not present' do
+        shed = create(:shed)
+
+        shed.update(code: '')
+
+        expect(shed).not_to be_valid
+        expect(shed.errors.full_messages.length).to eq 2
+        expect(shed.errors.full_messages.first).to eq 'Código não pode ficar em branco'
+        expect(shed.errors.full_messages.last).to eq 'Código não possui o tamanho esperado (3 caracteres)'
+      end
+
+      it 'invalid when city is not present' do
+        shed = create(:shed)
+
+        shed.update(city: '')
+
+        expect(shed).not_to be_valid
+        expect(shed.errors.full_messages.length).to eq 1
+        expect(shed.errors.full_messages.last).to eq 'Cidade não pode ficar em branco'
+      end
+
+      it 'invalid when area is not present' do
+        shed = create(:shed)
+
+        shed.update(area: '')
+
+        expect(shed).not_to be_valid
+        expect(shed.errors.full_messages.length).to eq 2
+        expect(shed.errors.full_messages.first).to eq 'Área não pode ficar em branco'
+        expect(shed.errors.full_messages.last).to eq 'Área não é um número'
+      end
+
+      it 'invalid when address is not present' do
+        shed = create(:shed)
+
+        shed.update(address: '')
+
+        expect(shed).not_to be_valid
+        expect(shed.errors.full_messages.length).to eq 1
+        expect(shed.errors.full_messages.last).to eq 'Endereço não pode ficar em branco'
+      end
+
+      it 'invalid when postcode is not present' do
+        shed = create(:shed)
+
+        shed.update(postcode: '')
+
+        expect(shed).not_to be_valid
+        expect(shed.errors.full_messages.length).to eq 2
+        expect(shed.errors.full_messages.first).to eq 'Código postal não pode ficar em branco'
+        expect(shed.errors.full_messages.last).to eq 'Código postal deve ter o formato: 00000-000'
+      end
+
+      it 'invalid when description is not present' do
+        shed = create(:shed)
+
+        shed.update(description: '')
+
+        expect(shed).not_to be_valid
+        expect(shed.errors.full_messages.length).to eq 1
+        expect(shed.errors.full_messages.last).to eq 'Descrição não pode ficar em branco'
+      end
+
+      it 'invalid when code is already in use' do
+        first_shed = create(:shed, code: 'ASU')
+        second_shed = create(:shed, code: 'USA')
+
+        second_shed.update(code: first_shed.code)
+
+        expect(second_shed.errors.full_messages.length).to eq 1
+        expect(second_shed.errors.full_messages.last).to eq 'Código já está em uso'
+        expect(second_shed.reload.code).to eq 'USA'
+      end
+
+      it 'invalid when name is already in use' do
+        first_shed = create(:shed, name: 'SP')
+        second_shed = create(:shed, name: 'MG')
+
+        second_shed.update(name: first_shed.name)
+
+        expect(second_shed.errors.full_messages.length).to eq 1
+        expect(second_shed.errors.full_messages.last).to eq 'Nome já está em uso'
+        expect(second_shed.reload.name).to eq 'MG'
+      end
+
+      it 'invalid when the area is not greater than 20' do
+        shed = create(:shed)
+
+        shed.update(area: 1)
+
+        expect(shed.errors.full_messages.length).to eq 1
+        expect(shed.errors.full_messages.last).to eq 'Área deve ser maior que 20'
+      end
+
+      it 'invalid when code length is not 3' do
+        error_message = 'Código não possui o tamanho esperado (3 caracteres)'
+        first_shed = create(:shed, code: 'SUV')
+        second_shed = create(:shed, code: 'SEC')
+
+        first_shed.update(code: 'ASUD')
+        second_shed.update(code: 'AS')
+
+        expect(first_shed.errors.full_messages.length).to eq 1
+        expect(second_shed.errors.full_messages.length).to eq 1
+        expect(first_shed.errors.full_messages.last).to eq error_message
+        expect(second_shed.errors.full_messages.last).to eq error_message
+      end
+
+      it 'invalid when postcode does not match format' do
+        shed = create(:shed, postcode: %w[01234-567 08652-300 78451-123].sample)
+
+        shed.update(postcode: '451165415469')
 
         expect(shed).not_to be_valid
         expect(shed.errors.full_messages.length).to eq 1
