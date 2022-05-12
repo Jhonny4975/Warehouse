bb2c61118a320fd3143bf2ed7b241560c610e4ba
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-12 14:43:16 -0300

    add validations

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class Supplier < ApplicationRecord
+  validates :corporate_name,
+            :brand_name,
+            :registration_number,
+            :email, presence: true
+
+  validates :brand_name,
+            :registration_number,
+            :email, uniqueness: true
+
+  validates :registration_number, format: { with: %r{\A\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}\z},
+                                            message: 'deve ter o formato: 00.000.000/0000-00' }
+
+  validates :phone_number, format: { with: /\A\(\d{2}\) \d{5}-\d{4}\z/,
+                                     message: 'deve ter o formato: (12) 12345-1234' }
 end
