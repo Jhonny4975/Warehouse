3afb0cb1aca2063367c640e13c17b611f98e5070
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-12 14:40:34 -0300

    Update suppliers_controller.rb

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -14,4 +14,35 @@ class SuppliersController < ApplicationController
   def show
     @supplier = Supplier.find(params[:id])
   end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+
+    if @supplier.save
+      redirect_to @supplier, notice: 'Fornecedor cadastrado com sucesso!'
+    else
+      flash.now[:notice] = 'Fornecedor não cadastrado.'
+
+      render :new
+    end
+  end
+
+  private
+
+  def supplier_params
+    params.require(:supplier).permit(
+      :corporate_name,
+      :brand_name,
+      :registration_number,
+      :street_address,
+      :city,
+      :state,
+      :email,
+      :phone_number
+    )
+  end
 end
